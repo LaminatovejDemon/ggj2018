4bfe21318c08eb2b97bdd7c6c73c72e4afa70bd6
Plysovej Kaktus <filip.pokorny@solfa.uk>
2018-01-26 21:13:19 +0000

    FP: Microphone recorder alpha

---
+++ Assets/scripts/microphoneReader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class microphoneReader : MonoBehaviour {
+
+	string _microphone;
+	public AudioSource _audio;
+	MicrophoneState _state;
+
+	enum MicrophoneState{
+		Off,
+		Initialized,
+		Recording,
+	};
+
+	void InitAudio(){
+		if (_state != MicrophoneState.Off) {
+			return;
+		} 
+
+		for (int i = 0; i < Microphone.devices.Length; ++i) {
+			if (Microphone.devices [i] != null) {
+				_microphone = Microphone.devices [i];
+				Debug.Log ("Our first device is " + _microphone);
+				_audio.clip = Microphone.Start (_microphone, true, 16, 44100);
+				_audio.loop = true;
+				_state = MicrophoneState.Initialized;
+			}
+		}
+	}
+
+	void StartRecording(){
+		if (_state != MicrophoneState.Initialized) {
+			return;
+		}
+
+		if ( !_audio.isPlaying && Microphone.IsRecording(_microphone) ){
+			if (Microphone.GetPosition (_microphone) == 0) {
+				Debug.Log ("not recording");
+				return;
+			}
+
+			_audio.Play ();
+			_state = MicrophoneState.Recording;
+		}
+	}
+		
+	void Update () {
+		InitAudio ();
+		StartRecording ();
+	}
+		
+	public float GetMicrophoneValue(){
+		if (_state != MicrophoneState.Recording) {
+			return 0;
+		}
+
+		float[] data = new float[735];
+		_audio.GetOutputData (data, 0);
+		ArrayList _array = new ArrayList ();
+		for (int i = 0; i < 735; ++i) {
+			_array.Add(Mathf.Abs(data[i]));
+		}
+		_array.Sort();
+		return (float)_array [735 / 2];
+	}
+}
