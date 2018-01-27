b890a227b21f72a0fa4d6a2370c7f5e5e6fd5572
xxxmatulisxxx <xxxmatulisxxx@gmail.com>
2018-01-27 01:13:18 +0000

    sound line temp copy

--- Assets/SoundLine.cs
+++ Assets/SoundLine.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SoundLine : MonoBehaviour {
 
-    int LineCubeNom = 512;
+    int LineCubeNom = 1024;
     int frameLine = 0;
     int increamentPerFrame = 10;
 
     GameObject[] line;
     public GameObject template;
 
-    float target = 1f;
+    float target = -0f;
     float increamentSpeed = 0.5f;
     
 	void Start () {
@@ -23,6 +23,7 @@ public class SoundLine : MonoBehaviour {
             line [i] = GameObject.Instantiate(template);
             line[i].transform.parent = gameObject.transform;
             line[i].transform.position = screen + Vector3.right * step_ * i;
+            line[i].transform.localPosition = new Vector3(line[i].transform.localPosition.x, target, line[i].transform.localPosition.z);
 
         }
         
@@ -32,34 +33,35 @@ public class SoundLine : MonoBehaviour {
 
     void Update () {
 
-        // target = microphoneMngr.instance.GetMicrophoneValue() * 1000.0f;
-        target = 0 - 2;
+        target = (microphoneMngr.instance.GetMicrophoneValue() * 500.0f)-2;
+        if (target > 2)target = 2;
         if (frameLine > LineCubeNom-increamentPerFrame) frameLine = 0;
         Debug.Log("" + target);
-        increamentPerFrame = (int)(100 * Time.deltaTime);
+        increamentPerFrame = (int)(200 * Time.deltaTime);
         for (int i = 0; i < increamentPerFrame; ++i)
         {
             if (frameLine != 0)
             {
-                if (line[frameLine + i - 1].transform.localPosition.y < target - 0.5f)
+                if (line[frameLine + i - 1].transform.localPosition.y <= target - (increamentSpeed/2))
                 {
-                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y + increamentSpeed, 0);
+                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y + increamentSpeed, line[frameLine + i].transform.localPosition.z);
                 }
-                else if (line[frameLine + i - 1].transform.localPosition.y > target + 0.5f)
+                else if (line[frameLine + i - 1].transform.localPosition.y >= target + (increamentSpeed / 2))
                 {
-                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y - increamentSpeed, 0);
+                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y - increamentSpeed, line[frameLine + i].transform.localPosition.z);
 
                 }
+                else line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y, line[frameLine + i].transform.localPosition.z);
 
             }
             else {
-                if (line[LineCubeNom - 1].transform.localPosition.y < target)
+                if (line[LineCubeNom - 1].transform.localPosition.y < target )
                 {
-                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[LineCubeNom - 1].transform.localPosition.y + increamentSpeed, 0);
+                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[LineCubeNom - 1].transform.localPosition.y + increamentSpeed, line[frameLine + i].transform.localPosition.z);
                 }
                 else if (line[LineCubeNom - 1].transform.localPosition.y > target)
                 {
-                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[LineCubeNom - 1].transform.localPosition.y - increamentSpeed, 0);
+                    line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[LineCubeNom - 1].transform.localPosition.y - increamentSpeed, line[frameLine + i].transform.localPosition.z);
 
                 }
             }
