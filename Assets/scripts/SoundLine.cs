1fdd6d296c6b768fc71a78d09bf7d157dd975456
Plysovej Kaktus <filip.pokorny@solfa.uk>
2018-01-27 04:46:46 +0000

    FP: Segment manager alpha

--- Assets/scripts/SoundLine.cs
+++ Assets/scripts/SoundLine.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SoundLine : MonoBehaviour {
 
-    int LineCubeNom = 1000;
+    int LineCubeNom = 1024;
     int frameLine = 0;
     int increamentPerFrame = 10;
     public float maxSoundLocation;
@@ -26,13 +26,46 @@ public class SoundLine : MonoBehaviour {
             line[i].transform.parent = gameObject.transform;
             line[i].transform.position = screen + Vector3.right * step_ * i;
             line[i].transform.localPosition = new Vector3(line[i].transform.localPosition.x, target, line[i].transform.localPosition.z);
+/*			Vector2 scaleBak_ = line [i].transform.localScale;
+			scaleBak_.x = step_;
+			line [i].transform.localScale = scaleBak_;*/
 
         }
-        
-
     }
 
+	void DecayColors(){
+		Material current_;
+		for (int i = 0; i < LineCubeNom; ++i) {
+			current_ = line [i].transform.GetChild(0).GetComponent<Renderer> ().material;
+			Color color_ = current_.GetColor ("_TintColor");
+			if ( color_.a > 0 ){
+				color_.a -= Time.deltaTime;
+				if (color_.a < 0) {
+					color_.a = 0;
+				}
+				if (i == 0) {
+					Debug.Log ("Decreasing to " + color_.a);
+				}
 
+			}
+			current_.SetColor ("_TintColor", color_);
+		}
+	}
+
+	void BoostColor(int index){
+		Material current_ = line [index].transform.GetChild(0).GetComponent<Renderer> ().material;
+		Color color_ = current_.GetColor ("_TintColor");
+		color_.a = 1;
+		current_.SetColor ("_TintColor", color_);
+	}
+
+	void ScaleCube(GameObject cube, GameObject reference)
+	{
+		Vector3 scale_ = cube.transform.localScale;
+		scale_.y = reference.transform.position.y - cube.transform.position.y;
+		cube.transform.localScale = scale_;
+	}
+		
     void Update () {
 
         target = (microphoneMngr.instance.GetMicrophoneValue() * 50.0f)-2;
@@ -46,9 +79,11 @@ public class SoundLine : MonoBehaviour {
 		increamentPerFrame = (int)(250 * Time.deltaTime);
 		if (frameLine > LineCubeNom - increamentPerFrame - 1) frameLine = 0;
 
+		DecayColors ();
+
         for (int i = 0; i < increamentPerFrame; ++i)
         {
-            
+			BoostColor (frameLine + i);
             if (frameLine != 0)
             {
                 oldPosition = line[frameLine + i].transform.localPosition.y;
@@ -63,6 +98,7 @@ public class SoundLine : MonoBehaviour {
                 }
                 else line[frameLine + i].transform.localPosition = new Vector3(line[frameLine + i].transform.localPosition.x, line[frameLine + i - 1].transform.localPosition.y, line[frameLine + i].transform.localPosition.z);
 
+//				ScaleCube (line [frameLine + i - 1], line [frameLine + i]);
             }
             else {
                 if (line[LineCubeNom - 1].transform.localPosition.y < target )
@@ -82,5 +118,7 @@ public class SoundLine : MonoBehaviour {
 
         }
         frameLine += increamentPerFrame;
+
+		shoutMngr.instance.SetSweep (frameLine / (float)LineCubeNom);
 	}
 }
---
+++ Assets/scripts/shoutMngr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoutMngr : baseMngr<shoutMngr> {
+
+	public int _segmentCount = 4;
+	public GameObject _segmentPrefab;
+	GameObject [] _segment;
+	public float _segmentGap = 0.1f;
+
+	public Color _activeColor;
+	public Color _inactiveColor;
+
+	int _activeIndex = 0;
+
+	void Start(){
+		Initialise ();
+	}
+
+	public void Initialise(){
+		if (_segment != null) {
+			return;
+		}
+		_segment = new GameObject[_segmentCount];
+		for (int i = 0; i < _segmentCount; ++i) {
+			_segment[i] = GameObject.Instantiate(_segmentPrefab);
+			_segment[i].transform.localScale = new Vector3 (Camera.main.orthographicSize * 2 * Camera.main.aspect / _segmentCount - _segmentGap, Camera.main.orthographicSize * 2, 1);
+			_segment[i].transform.parent = Camera.main.transform;
+			_segment[i].transform.position = Camera.main.ViewportToWorldPoint (Vector3.zero) + Vector3.right * (_segment [i].transform.localScale.x + _segmentGap) * i + Vector3.back * 1.0f;
+			_segment[i].transform.GetChild(0).GetComponent<Renderer> ().material.SetColor ("_TintColor", _inactiveColor);
+		}
+	}
+
+	void SwitchSegment(int newIndex){
+		_segment [_activeIndex].transform.GetChild(0).GetComponent<Renderer> ().material.SetColor ("_TintColor", _inactiveColor);
+		_activeIndex = newIndex;
+		_segment [_activeIndex].transform.GetChild(0).GetComponent<Renderer> ().material.SetColor ("_TintColor", _activeColor);
+	}
+
+	public void SetSweep(float position){
+		int segment_ = (int)(_segmentCount * position);
+		if (segment_ != _activeIndex) {
+			SwitchSegment (segment_);
+		}
+	}
+}
